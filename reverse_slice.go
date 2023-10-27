b7a7e3df6f7e4be17ab93037e8d09e3e1f5d9873
MHQA-138171 <omegameen1@gmail.com>
2023-10-27 07:00:57 +0500

    writing package

---
+++ reverse_slice.go
@@ -0,0 +1,15 @@
+package reverseslice
+
+import "errors"
+
+func ReverseSlice(slice []int) error {
+	if slice == nil {
+		return errors.New("the array passed is empty")
+	}
+	for i := len(slice); i >= 0; i-- {
+		for j := 0; i < len(slice); j++ {
+			slice[j] = slice[i]
+		}
+	}
+	return nil
+}
