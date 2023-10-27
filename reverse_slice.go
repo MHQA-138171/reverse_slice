dc8adb11e3656ae5c319756b3fe9ace9e943fbdf
MHQA-138171 <omegameen1@gmail.com>
2023-10-27 08:11:01 +0500

    adding guard clause for empty slice

--- reverse_slice.go
+++ reverse_slice.go
@@ -6,9 +6,12 @@ import (
 
 func ReverseSlice(slice []int) error {
 	if slice == nil {
-		return errors.New("the slice passed is empty")
+		return errors.New("the slice passed nil")
 	}
 	LEN := len(slice)
+	if LEN == 0 {
+		return errors.New("the slice passed is empty")
+	}
 	temp := make([]int, LEN)
 	j := 0
 	for i := LEN - 1; i >= 0; i-- {
