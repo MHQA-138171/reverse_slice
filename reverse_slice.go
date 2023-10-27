08601a011abe9b1c721f2edc274517a4d1f2e04c
MHQA-138171 <omegameen1@gmail.com>
2023-10-27 07:19:04 +0500

    fixing bug

--- reverse_slice.go
+++ reverse_slice.go
@@ -1,11 +1,15 @@
 package reverseslice
 
-import "errors"
+import (
+	"errors"
+	"fmt"
+)
 
 func ReverseSlice(slice []int) error {
 	if slice == nil {
 		return errors.New("the array passed is empty")
 	}
+	fmt.Println(len(slice))
 	startingIndex := len(slice) - 1
 	for i := startingIndex; i >= 0; i-- {
 		for j := 0; i < len(slice); j++ {
